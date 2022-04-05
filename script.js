ba405d141dab45b02858b16ec41f4cccd3617ecd
monsoonforest <chintz604@gmail.com>
2022-04-05 14:10:49 +0530

    back to old

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ map.attributionControl.addAttribution('Data &copy; <a href="https://indiawris.go
 $.getJSON("dams_wri_table_20220331.geojson", function (data) {
   var dams = L.geoJson(data, {
         pointToLayer: function( feature, latlng) {
-          return L.marker(latlng, { options: {
+          return L.icon(latlng, { options: {
             iconUrl: 'dams.png',
             iconSize:     [5, 10]
                      }          })
@@ -46,7 +46,7 @@ $.getJSON("dams_wri_table_20220331.geojson", function (data) {
 $.getJSON("barrages_wri_table_20220331.geojson", function (data) {
   var barrages = L.geoJson(data, {
         pointToLayer: function( feature, latlng) {
-          return L.marker(latlng, { options: {
+          return L.icon(latlng, { options: {
             iconUrl: 'barrages.png',
             iconSize:     [5, 10]
             
@@ -65,60 +65,60 @@ $.getJSON("barrages_wri_table_20220331.geojson", function (data) {
 // 	}
 // }
 
-// L.geoJson(barrages, {
-// 	style: setStyle
-// }).addTo(map);
+// // L.geoJson(barrages, {
+// // 	style: setStyle
+// // }).addTo(map);
 
-// L.geoJson(dams, {
-// 	style: setStyle
-// }).addTo(map);
+// // L.geoJson(dams, {
+// // 	style: setStyle
+// // }).addTo(map);
 
-for (var num = 0; num < barrages.length; num++) {
-	// Grab information on the barrages we are currently looping through
-	var barrage = barrages[num];
-	var barrage_lat = barrage["Latitude"];
-	var barrage_long = barrage["Longitude"];
-	var barrage_name = barrage["barrage"];
-	var barrage_state = barrage["State"];
-	var barrage_river = barrage["River"];
-	var barrage_year = barrage["Year_of_Completion"];
+// for (var num = 0; num < barrages.length; num++) {
+// 	// Grab information on the barrages we are currently looping through
+// 	var barrage = barrages[num];
+// 	var barrage_lat = barrage["Latitude"];
+// 	var barrage_long = barrage["Longitude"];
+// 	var barrage_name = barrage["barrage"];
+// 	var barrage_state = barrage["State"];
+// 	var barrage_river = barrage["River"];
+// 	var barrage_year = barrage["Year_of_Completion"];
 
-	// Use Leaflet to add a marker for each barrage
-	// And give it the lat, long information
-	// In the current barrage's object
-	var marker = L.marker([barrage_lat, barrage_long]).addTo(map);
+// 	// Use Leaflet to add a marker for each barrage
+// 	// And give it the lat, long information
+// 	// In the current barrage's object
+// 	var marker = L.marker([barrage_lat, barrage_long]).addTo(map);
 	
-	// HTML that will appear in popup
-	var popup_html = '<h3>' + barrage_name + '</h3>';
-	popup_html += '<div>' + barrage_state + '</div>';
-	popup_html += '<div>' + barrage_river + '</div>';
-    popup_html += '<div>' + barrage_year + '</div>';
+// 	// HTML that will appear in popup
+// 	var popup_html = '<h3>' + barrage_name + '</h3>';
+// 	popup_html += '<div>' + barrage_state + '</div>';
+// 	popup_html += '<div>' + barrage_river + '</div>';
+//     popup_html += '<div>' + barrage_year + '</div>';
 	
-	// Bind the popup to the marker using Leaflet
-	marker.bindPopup(popup_html);
-}
+// 	// Bind the popup to the marker using Leaflet
+// 	marker.bindPopup(popup_html);
+// }
 
-for (var num = 0; num < dams.length; num++) {
-	// Grab information on the dams we are currently looping through
-	var name = dams[num];
-	var dam_lat = dam["Latitude"];
-	var dam_long = dam["Longitude"];
-	var dam_name = dam["dam"];
-	var dam_state = dam["State"];
-	var dam_river = dam["River"];
-	var dam_year = dam["Year_of_Completion"];
+// for (var num = 0; num < dams.length; num++) {
+// 	// Grab information on the dams we are currently looping through
+// 	var name = dams[num];
+// 	var dam_lat = dam["Latitude"];
+// 	var dam_long = dam["Longitude"];
+// 	var dam_name = dam["dam"];
+// 	var dam_state = dam["State"];
+// 	var dam_river = dam["River"];
+// 	var dam_year = dam["Year_of_Completion"];
 
-	// Use Leaflet to add a marker for each dam
-	// And give it the lat, long information
-	// In the current dam's object
-	var marker = L.marker([dam_lat, dam_long]).addTo(map);
+// 	// Use Leaflet to add a marker for each dam
+// 	// And give it the lat, long information
+// 	// In the current dam's object
+// 	var marker = L.marker([dam_lat, dam_long]).addTo(map);
 	
-	// HTML that will appear in popup
-	var popup_html = '<h3>' + dam_name + '</h3>';
-	popup_html += '<div>' + dam_state + '</div>';
-	popup_html += '<div>' + dam_river + '</div>';
-    popup_html += '<div>' + dam_year + '</div>';
+// 	// HTML that will appear in popup
+// 	var popup_html = '<h3>' + dam_name + '</h3>';
+// 	popup_html += '<div>' + dam_state + '</div>';
+// 	popup_html += '<div>' + dam_river + '</div>';
+//     popup_html += '<div>' + dam_year + '</div>';
 	
-	// Bind the popup to the marker using Leaflet
-	marker.bindPopup(popup_html);
-}
+// 	// Bind the popup to the marker using Leaflet
+// 	marker.bindPopup(popup_html);
+// }
