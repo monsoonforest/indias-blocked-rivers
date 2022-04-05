8f34ec2076c5737491635e2c39112a418907f7ca
monsoonforest <chintz604@gmail.com>
2022-04-05 11:42:25 +0530

    back to old

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Edit the center point and zoom level
-var map = L.map('map', {
+var map = L.Map('map', {
   center: [20.34, 77.89],
   zoom: 5  ,
   minZoom : 5,
@@ -8,11 +8,11 @@ var map = L.map('map', {
 
 
 
-// // layer controls
-// var controlLayers = L.control.layers( null, null, {
-//      position:"topleft",
-//      collapsed: true // truw = closed by default
-//     }).addTo(map);
+// layer controls
+var controlLayers = L.control.layers( null, null, {
+     position:"topleft",
+     collapsed: false // truw = closed by default
+    }).addTo(map);
 
 // new L.tileLayer('https://{s}.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png', {
 //   attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -26,8 +26,8 @@ map.attributionControl.addAttribution('View <a href="https://github.com/monsoonf
 map.attributionControl.addAttribution('Data &copy; <a href="https://indiawris.gov.in/wris/#/waterResources">Gov. of India </a>');
 
 
-var imagery = new L.esri.basemapLayer('Imagery').addTo(map);
-var imagerylabels = new L.esri.basemapLayer('ImageryLabels').addTo(map);
+new L.esri.basemapLayer('Imagery').addTo(map);
+new L.esri.basemapLayer('ImageryLabels').addTo(map);
 
 
 // $.getJSON("dams_wri_table_20220331.geojson", function (data) {
